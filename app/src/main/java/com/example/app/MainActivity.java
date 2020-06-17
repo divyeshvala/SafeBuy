08988ba2d7eed04045b8cdc08be5cfad003dde6c
divyeshvala <divyeshvala228@gmail.com>
2020-06-17 08:33:01 +0530

    Testing commit

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Just testing
+        for(int i=0; i<5; i++)
+            Log.i("MainActivity", i+" ");
     }
 }
