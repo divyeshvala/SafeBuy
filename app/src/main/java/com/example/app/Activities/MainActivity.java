9120d6997229eb434d0dc8239c9c0a5c9dc0b221
divyeshvala <divyeshvala228@gmail.com>
2020-06-18 14:51:00 +0530

    Realtime database created

--- app/src/main/java/com/example/app/Activities/MainActivity.java
+++ app/src/main/java/com/example/app/Activities/MainActivity.java
@@ -5,14 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.app.R;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
     }
 }
