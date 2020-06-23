ef566f8c6787fbdea7570941a316ed29e2a2f7ec
KUNAL ANAND <anandkunal241@gmail.com>
2020-06-24 01:36:27 +0530

    Login and signup almost complete

--- app/src/main/java/com/example/app/Activities/Login.java
+++ app/src/main/java/com/example/app/Activities/Login.java
@@ -1,26 +1,61 @@
 package com.example.app.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.app.R;
 import com.example.app.fragment.FragmentLogin;
 import com.example.app.fragment.FragmentSignUp;
 import com.example.app.fragment.LoginSignupFragmentAdapter;
 import com.example.app.ui.login.SectionsPagerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 //Login Activity has fragments for both sigin and signup
 public class Login extends AppCompatActivity
 {
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // This part whether the user is currently logged in
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if(firebaseUser != null){
+                    // if user is logged in
+                    // goto main activity
+
+                    Toast.makeText(Login.this, "you are logged in", Toast.LENGTH_LONG);
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(Login.this, "Log in", Toast.LENGTH_LONG);
+                }
+
+            }
+        };
+
+        // if user is not logged  you need to login or signup
 
         LoginSignupFragmentAdapter adapter = new LoginSignupFragmentAdapter(getSupportFragmentManager());
         adapter.addFragment(FragmentLogin.newInstance(), "Login");
@@ -46,13 +81,13 @@ public class Login extends AppCompatActivity
             }
         });
 
-        new Handler().postDelayed(new Runnable() {
+        /*new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(Login.this, MainActivity.class));
                 finish();
             }
-        }, 5000);
+        }, 5000);*/
 
     }
 }
--- app/src/main/java/com/example/app/fragment/FragmentSignUp.java
+++ app/src/main/java/com/example/app/fragment/FragmentSignUp.java
@@ -1,19 +1,40 @@
 package com.example.app.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.app.Activities.Login;
+import com.example.app.Activities.MainActivity;
+import com.example.app.Activities.SignUpActivity;
 import com.example.app.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class FragmentSignUp extends Fragment {
+public class FragmentSignUp extends Fragment implements View.OnClickListener {
+
+    private TextInputLayout txtEmail, txtPassword;
+    private Button btnLogin_register;
+    private ProgressBar progressBar;
+    private FirebaseAuth firebaseAuth;
+
 
     public static FragmentSignUp newInstance() {
+
         return new FragmentSignUp();
     }
 
@@ -21,6 +42,76 @@ public class FragmentSignUp extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signup, container, false);
+
+        txtEmail = (TextInputLayout) view.findViewById(R.id.username);
+        txtPassword = (TextInputLayout) view.findViewById(R.id.password);
+        btnLogin_register = (Button) view.findViewById(R.id.login_register);
+        progressBar = (ProgressBar) view.findViewById(R.id.loading);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        System.out.println("Entered Signup fragement");
+
+        btnLogin_register.setOnClickListener(this);
+
         return view;
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+
+        System.out.println("Button clicked");
+
+        switch(v.getId()){
+
+            case R.id.login_register:
+
+                    String email = txtEmail.getEditText().getText().toString().trim();
+                    String password = txtPassword.getEditText().getText().toString().trim();
+
+                    System.out.println("Email : " + email);
+                    System.out.println("Password : " + password);
+
+                    if(TextUtils.isEmpty(email)){
+                        Toast.makeText(getActivity(),"Please Enter Email", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    if(TextUtils.isEmpty(password)){
+                        Toast.makeText(getActivity(), "Please Enter Password", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    if(password.length() < 6){
+                        Toast.makeText(getActivity(), "Password is too short", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    progressBar.setVisibility(View.VISIBLE);
+
+
+                    firebaseAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    if (task.isSuccessful()) {
+                                        startActivity(new Intent(getActivity(), Login.class));
+                                        Toast.makeText(getActivity(), "Sign Up Successful", Toast.LENGTH_SHORT).show();
+
+                                    } else {
+                                        Toast.makeText(getActivity(), "Authentication Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                    break;
+
+            default: break;
+        }
+
     }
 }
