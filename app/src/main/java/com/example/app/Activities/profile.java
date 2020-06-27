cfb360b0dc0308a8de52ae414c188617b920aa2b
sanat <sanathramesh55@gmail.com>
2020-06-27 12:26:18 +0530

    restructured profile and added transactions

---
+++ app/src/main/java/com/example/app/Activities/profile.java
@@ -0,0 +1,23 @@
+package com.example.app.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.app.R;
+import com.example.app.fragment.customer_profile;
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.holder,new customer_profile()).addToBackStack(null).commit();
+
+
+    }
+}
