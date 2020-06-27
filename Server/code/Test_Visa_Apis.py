dce9dbfae6b51ed9a7eb2dc7e05646bbcf5c2ae1
KUNAL ANAND <anandkunal241@gmail.com>
2020-06-27 17:03:22 +0530

    Merchant locator Server has been updated

--- Server/code/Test_Visa_Apis.py
+++ Server/code/Test_Visa_Apis.py
@@ -1,28 +1,39 @@
-# Code for Testing Global ATM Locator API
+# Code for Testing VISA Direct
 
 import requests
 import json
 import os
+import datetime
 
-url = 'https://sandbox.api.visa.com/merchantlocator/v1/locator'
+now = datetime.datetime.now()
+
+print(now)
+
+date = now.strftime("%Y-%m-%dT%H:%M:%S")
+
+print(date)
+
+
+url = 'https://sandbox.api.visa.com/visadirect/fundstransfer/v1/pushfundstransactions'
 
 
 headers = {'Accept': 'application/json'}
 
-json_file = open('../Resources/merchant_locator_payload.json',)
+json_file = open('../Resources/visa_direct_payload.json',)
 
 payload = json.load(json_file)
 
 
-user_id = 'XC2ZRLC7JCX0B2PR5SWK21t2Or5cIWdZsdfe9DAw4vfq30040'
+user_id = '442BYE64PK21LL73EKHO21ao5rYfOa0rYd1zFG4-4h6CAnqVA'
 
-password = 'BdjGC5WegJZ13qit60o2'
+password = '1144Bv6dOv2rS31kooTm'
 
-key = '../Resources/merchant_locator_api_key.pem'
+key = '../Resources/visa_direct_api_key.pem'
 
-cert = '../Resources/merchant_locator_api_cert.pem'
+cert = '../Resources/visa_direct_api_cert.pem'
 
 resp = requests.post(url=url, cert=(cert, key), auth=(user_id, password), headers=headers, json=payload, timeout=10)
 
+print(resp)
 data = json.loads(resp.content)
 print(data)
