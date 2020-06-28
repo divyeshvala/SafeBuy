b087649c37b243145fbf55c0e7b76555fd55ce3d
KUNAL ANAND <anandkunal241@gmail.com>
2020-06-28 23:25:52 +0530

    payment server updated

--- Server/code/PushPaymentHandler.py
+++ Server/code/PushPaymentHandler.py
@@ -1,12 +1,10 @@
-import requests
-import json
-
 from Server.code.TransactionVerificationHandler import handlePaymentRequest
 
 
 def handlePaymentClient(curr_request, root, tablepath1, tablepath2):
     if 'gotResponse' not in curr_request and curr_request['gotResponse'] == 'false':
 
-        handlePaymentRequest(root, tablepath1, curr_request['senderPAN'], curr_request['receiverPAN'])
+        handlePaymentRequest(root, tablepath1, curr_request['senderPAN'], curr_request['receiverPAN']
+                             , curr_request['amount'], curr_request['transactionCurrencyCode'])
         root.child(tablepath2).update({'gotResponse': 'true'})
 
