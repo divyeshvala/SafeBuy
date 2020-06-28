a5af8218f700819088c340dcb432a15a7448299b
KUNAL ANAND <anandkunal241@gmail.com>
2020-06-28 15:21:06 +0530

    PaymentHandler almost complete

--- app/src/main/java/com/example/app/Utilities/PaymentHandler.java
+++ app/src/main/java/com/example/app/Utilities/PaymentHandler.java
@@ -1,19 +1,29 @@
 package com.example.app.Utilities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.EventListener;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PaymentHandler {
 
     private static final String TAG = "PaymentHandler";
-    private final Context context;
-    private final String receiverPAN, senderPAN;
+    private Context context;
+    private String receiverPAN, senderPAN;
+    private String paymentResponse;
 
     public PaymentHandler(Context context, String senderPAN, String receiverPAN){
         this.context = context;
@@ -37,6 +47,53 @@ public class PaymentHandler {
 
         final DatabaseReference responseTable = FirebaseDatabase.getInstance()
                 .getReference().child("PaymentRequest").child(databaseReference.getKey());
+
+        // Got the Payment result
+        responseTable.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child("gotResponse").getValue(String.class)!=null){
+
+                    if(dataSnapshot.child("gotResponse").getValue(String.class).equals("true")){
+                        FirebaseDatabase.getInstance().getReference()
+                                .child("PaymentRequest").child(databaseReference.getKey()).removeValue();
+
+                        Intent intent = new Intent("got Response");
+
+                        context.sendBroadcast(intent);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //get the result
+
+        responseTable.child("Result").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                if(dataSnapshot.exists()){
+                    paymentResponse = dataSnapshot.child("response").getValue(String.class);
+
+                    Log.i(TAG, "got the Response" + paymentResponse);
+                }
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) { }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) { }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) { }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
+        });
 
     }
 }
