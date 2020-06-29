a81f319e26795edbfe1204b99acac28c65b858d6
divyeshvala <divyeshvala228@gmail.com>
2020-06-29 08:20:16 +0530

    Minor changes

--- app/src/main/java/com/example/app/fragment/customer_profile.java
+++ app/src/main/java/com/example/app/fragment/customer_profile.java
@@ -1,34 +1,41 @@
 package com.example.app.fragment;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import com.example.app.Activities.Login;
 import com.example.app.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.manojbhadane.PaymentCardView;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class customer_profile extends Fragment {
 
-    ImageView im;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
         View rootView = inflater.inflate(R.layout.fragment_customer_profile, container, false);
-        im = rootView.findViewById(R.id.transactions);
-        im.setOnClickListener(new View.OnClickListener(){
+
+        ImageView transactions = rootView.findViewById(R.id.transactions);
+        transactions.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
-
                 FragmentManager fx = getActivity().getSupportFragmentManager();
-//                FragmentManager fx = getFragmentManager();
                 fx.beginTransaction().replace(R.id.holder,new fragment_transactions()).addToBackStack(null).commit();
-
             }
         });
         
@@ -63,7 +70,7 @@ public class customer_profile extends Fragment {
                 startActivity(new Intent(getActivity(), Login.class));
             }
         });
+
         return rootView;
-        
     }
 }
