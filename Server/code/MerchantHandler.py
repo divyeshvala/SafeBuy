e086b48eec61d82befaeae912e61a96927514547
KUNAL ANAND <anandkunal241@gmail.com>
2020-06-30 21:43:27 +0530

    Exception raised in server are now being handled

--- Server/code/MerchantHandler.py
+++ Server/code/MerchantHandler.py
@@ -5,26 +5,27 @@ from Server.code.NearbyMerchantRequestHandler import handleMerchantRequests
 
 
 def handleMerchantClients(curr_request, root, tablepath1, tablepath2, tablepath3):
-
     if "resolvedContainment" in curr_request and curr_request["resolvedContainment"] == 'false':
 
-         print(curr_request)
+        print(curr_request)
 
-         dis = curr_request["distance"]
+        dis = curr_request["distance"]
 
-         if curr_request["distanceUnit"] == 'KM':
-             dis = dis + "000"
-
-         print(dis)
-
-         handleContainmentrequests(root, tablepath2, curr_request["longitude"], curr_request["latitude"],
-                                          dis)
-         root.child(tablepath3).update({'resolvedContainment': 'true'})
+        if curr_request["distanceUnit"] == 'KM':
+            dis = dis + "000"
+        try:
+            handleContainmentrequests(root, tablepath2, curr_request["longitude"], curr_request["latitude"],
+                                      dis)
+            root.child(tablepath3).update({'resolvedContainment': 'success'})
+        except:
+            root.child(tablepath3).update({'resolvedContainment': 'failure'})
 
     if "resolvedMerchant" in curr_request and curr_request["resolvedMerchant"] == 'false':
         print(curr_request["resolvedMerchant"])
 
-        handleMerchantRequests(root, tablepath1, curr_request["merchantCategoryCode"], curr_request["distance"],
-                                 curr_request["distanceUnit"], curr_request["latitude"], curr_request["longitude"])
-
-        root.child(tablepath3).update({'resolvedMerchant': 'true'})
+        try:
+            handleMerchantRequests(root, tablepath1, curr_request["merchantCategoryCode"], curr_request["distance"],
+                                   curr_request["distanceUnit"], curr_request["latitude"], curr_request["longitude"])
+            root.child(tablepath3).update({'resolvedMerchant': 'success'})
+        except:
+            root.child(tablepath3).update({'resolvedMerchant': 'failure'})
--- Server/code/PushPaymentHandler.py
+++ Server/code/PushPaymentHandler.py
@@ -4,7 +4,9 @@ from Server.code.TransactionVerificationHandler import handlePaymentRequest
 def handlePaymentClient(curr_request, root, tablepath1, tablepath2):
     if 'gotResponse' not in curr_request or curr_request['gotResponse'] == 'false':
 
-        handlePaymentRequest(root, tablepath1, curr_request['senderPAN'], curr_request['receiverPAN']
-                             , curr_request['amount'], curr_request['transactionCurrencyCode'])
-        root.child(tablepath2).update({'gotResponse': 'true'})
-
+        try:
+            handlePaymentRequest(root, tablepath1, curr_request['senderPAN'], curr_request['receiverPAN']
+                                 , curr_request['amount'], curr_request['transactionCurrencyCode'])
+            root.child(tablepath2).update({'gotResponse': 'success'})
+        except:
+            root.child(tablepath2).update({'gotResponse': 'failure'})
